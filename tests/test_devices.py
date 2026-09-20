TheoLeeCJ/SemIf

Add Apple Silicon support: PyTorch MPS backend and optional MLX readout
Run direct, serial, and shared scoring on Apple Silicon through two
backends that preserve the frozen prompt contracts and uncalibrated
probability warnings:

- PyTorch/MPS: device resolution (--device auto|cuda|mps) plus
  device-synchronized timings shared by all torch scorers. The shared
  scorer loops batch-1 suffixes on MPS, where batched multi-row suffix
  forwards are pathologically slow, and CUDA keeps its batched path.
- Optional MLX backend (--backend mlx): same pinned Qwen3.5-4B BF16
  checkpoint, no conversion. The loader validates checkpoint configs
  before any download, refuses custom model files (mlx-lm executes
  config-declared model_file as Python), quantized weights, and
  non-BF16 weights; FP32 is allowed only for linear-attention state
  parameters, verified by parameter name.
- CLI: reranker stays CUDA-only (rejected on MPS before loading);
  benchmark runners pin CUDA explicitly.
- MLX serial rows carry the same audit fields as torch serial rows
  (answer_token_ids, allowed_token_mass, full_vocab_argmax_id).
- Tests: CLI routing with mocked backends, device resolution, MLX
  loader guards, hybrid-cache branch isolation, MPS shared timing.
  All run without MLX installed (MLX tests skip) or a GPU.
- Docs: docs/APPLE_SILICON.md with install, run, guarantees, and
  measured M5 numbers (illustrative, not committed benchmarks).

Published CUDA results, claims, and checksums are untouched: 50 tests
pass, 19/19 SHA256SUMS OK, 69/69 published claims verified. MLX
quality comparison vs published CUDA: identical balanced accuracy on
authored144 (0.806) and every204 (0.942); wanli256 0.617 vs 0.637 and
typesafe102 modal agreement 0.820 vs 0.853 differ within the same
backend-independent range as MPS (see APPLE_SILICON.md).

Co-Authored-By: Claude Code <noreply@anthropic.com>

tests/test_devices.py
@@ -1,0 +1,106 @@
+from types import SimpleNamespace
+from unittest.mock import Mock
+import sys
+
+import pytest
+
+from openjev_phase1.core import load_causal_model, resolve_device, synchronize
+
+
+@pytest.fixture
+def torch_stub(monkeypatch):
+    torch = SimpleNamespace(
+        cuda=SimpleNamespace(is_available=Mock(return_value=False), device_count=Mock(return_value=0),
+                             synchronize=Mock()),
+        backends=SimpleNamespace(mps=SimpleNamespace(is_available=Mock(return_value=True))),
+        mps=SimpleNamespace(synchronize=Mock()),
+        device=lambda name: SimpleNamespace(type=name.split(":")[0], name=name),
+    )
+    monkeypatch.setitem(sys.modules, "torch", torch)
+    return torch
+
+
+def test_auto_uses_mps_without_cuda(torch_stub):
+    assert resolve_device().name == "mps"
+
+
+def test_auto_preserves_single_cuda_requirement(torch_stub):
+    torch_stub.cuda.is_available.return_value = True
+    torch_stub.cuda.device_count.return_value = 1
+    assert resolve_device().name == "cuda:0"
+    torch_stub.cuda.device_count.return_value = 2
+    with pytest.raises(ValueError, match="exactly one CUDA"):
+        resolve_device()
+
+
+def test_explicit_unavailable_devices_fail(torch_stub):
+    with pytest.raises(ValueError, match="exactly one CUDA"):
+        resolve_device("cuda")
+    torch_stub.backends.mps.is_available.return_value = False
+    with pytest.raises(ValueError, match="MPS is unavailable"):
+        resolve_device("mps")
+    with pytest.raises(ValueError, match="Device must"):
+        resolve_device("cpu")
+
+
+@pytest.mark.parametrize("backend", ["cuda", "mps", "cpu"])
+def test_synchronization_dispatch(torch_stub, backend):
+    device = SimpleNamespace(type=backend)
+    synchronize(device)
+    if backend == "cuda":
+        torch_stub.cuda.synchronize.assert_called_once_with(device)
+    else:
+        torch_stub.cuda.synchronize.assert_not_called()
+    if backend == "mps":
+        torch_stub.mps.synchronize.assert_called_once_with()
+    else:
+        torch_stub.mps.synchronize.assert_not_called()
+
+
+def test_loader_passes_device_dtype_and_revision(torch_stub, monkeypatch):
+    model = Mock()
+    factory = SimpleNamespace(from_pretrained=Mock(return_value=(model, {})))
+    config = SimpleNamespace(model_type="other")
+    transformers = SimpleNamespace(
+        AutoConfig=SimpleNamespace(from_pretrained=Mock(return_value=config)),
+        AutoTokenizer=SimpleNamespace(from_pretrained=Mock(return_value=object())),
+        AutoModelForCausalLM=factory, __version__="test",
+    )
+    monkeypatch.setitem(sys.modules, "transformers", transformers)
+    torch_stub.device = lambda name: name
+    torch_stub.float16 = "float16"
+    torch_stub.__version__ = "test"
+    revision = "a" * 40
+    _, _, metadata = load_causal_model("test/model", revision, "mps", "float16")
+    kwargs = factory.from_pretrained.call_args.kwargs
+    assert kwargs["device_map"] == {"": "mps"}
+    assert kwargs["dtype"] == "float16"
+    assert kwargs["revision"] == revision
+    assert kwargs["trust_remote_code"] is False
+    assert metadata["device"] == "mps"
+    assert metadata["dtype"] == "float16"
+    model.eval.assert_called_once()
+
+
+def test_direct_scorer_synchronizes_before_and_after_forward(monkeypatch):
+    import torch
+    from openjev_phase1 import direct
+
+    events = []
+
+    class Model:
+        def parameters(self):
+            yield torch.zeros(1)
+
+        def forward(self, input_ids, attention_mask, use_cache, return_dict, logits_to_keep):
+            events.append("forward")
+            return SimpleNamespace(logits=torch.tensor([[[1.0, 2.0]]]))
+
+        __call__ = forward
+
+    monkeypatch.setattr(direct, "encode_prompt", lambda *args: ([1], [0, 1], "hash"))
+    monkeypatch.setattr(direct, "synchronize", lambda device: events.append("sync"))
+    result = direct.score(Model(), None, {"id": "x", "options": [{"id": "a"}, {"id": "b"}]}, {})
+    assert events == ["sync", "forward", "sync"]
+    assert sum(result["probabilities"]) == pytest.approx(1.0)
+    assert result["option_logits"] == [1.0, 2.0]

tests/test_shared.py
@@ -1,6 +1,10 @@
+from types import SimpleNamespace
+
 import pytest
 
-from openjev_phase1.shared import _suffix_layout
+torch = pytest.importorskip("torch")
+
+from openjev_phase1.shared import _suffix_layout, score_shared  # noqa: E402
 
 
 def test_suffix_padding_follows_real_tokens():
@@ -14,3 +18,104 @@ def test_suffix_padding_follows_real_tokens():
 def test_empty_suffix_is_rejected():
     with pytest.raises(ValueError):
         _suffix_layout([[1], []], 7, 0)
+
+
+class FakeCache:
+    """Native-cache stand-in: mutable length, records reorder calls."""
+
+    def __init__(self, length):
+        self.length = length
+        self.orders = []
+
+    def get_seq_length(self):
+        return self.length
+
+    def reorder_cache(self, order):
+        self.orders.append(order.tolist() if hasattr(order, "tolist") else list(order))
+
+    def __deepcopy__(self, memo):
+        clone = FakeCache(self.length)
+        memo[id(self)] = clone
+        return clone
+
+
+class FakeModel:
+    """Forward with a real signature so selective-logits detection succeeds."""
+
+    def __init__(self, calls, device):
+        self.calls = calls
+        self._device = device
+
+    def parameters(self):
+        yield SimpleNamespace(device=self._device)
+
+    def eval(self):
+        return self
+
+    def forward(self, input_ids=None, attention_mask=None, use_cache=False,
+                return_dict=True, logits_to_keep=1, past_key_values=None):
+        width = int(input_ids.shape[1])
+        entry_length = None if past_key_values is None else past_key_values.length
+        self.calls.append({
+            "batch": int(input_ids.shape[0]),
+            "width": width,
+            "logits_to_keep": logits_to_keep,
+            "cache_entry_length": entry_length,
+            "reorders": None if past_key_values is None else len(past_key_values.orders),
+        })
+        if past_key_values is None:
+            past_key_values = FakeCache(width)
+        else:
+            past_key_values.length += width
+        logits = torch.arange(width + 4, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+        return SimpleNamespace(logits=logits, past_key_values=past_key_values)
+
+    __call__ = forward
+
+
+def test_score_shared_looped_path_forwards_one_row_per_call(monkeypatch):
+    if not torch.backends.mps.is_available():
+        pytest.skip("MPS unavailable")
+    rows_data = [
+        {"id": "a", "state": "s", "question": "q1", "options": [
+            {"id": "x", "description": "X"}, {"id": "y", "description": "Y"}]},
+        {"id": "b", "state": "s", "question": "q2", "options": [
+            {"id": "x", "description": "X"}, {"id": "y", "description": "Y"}]},
+    ]
+
+    class FakeTok:
+        pad_token_id = 0
+        eos_token_id = 1
+
+    calls = []
+
+    def fake_encode_prompt(tokenizer, row, max_tokens):
+        return ([1] * 8, [2, 3], f"hash-{row['id']}")
+
+    def fake_state_prefix(tokenizer, state):
+        return [1] * 6
+
+    import openjev_phase1.shared as shared_module
+    monkeypatch.setattr(shared_module, "encode_prompt", fake_encode_prompt)
+    monkeypatch.setattr(shared_module, "_state_prefix", fake_state_prefix)
+    results, timing = score_shared(FakeModel(calls, torch.device("mps")), FakeTok(), rows_data, {}, 4096)
+
+    assert [r["id"] for r in results] == ["a", "b"]
+    assert timing["batch_size"] == 2
+    assert timing["suffix_forward_seconds"] > 0
+    assert timing["replicate_seconds"] > 0
+    assert all(r["model"]["serving_config"] == "native-state-prefix-looped-v1" for r in results)
+    # Looped suffixes are unpadded: padded count equals true suffix tokens.
+    assert timing["padded_suffix_tokens"] == timing["true_suffix_tokens"] == 4
+    assert len(calls) == 3  # one prefill + one suffix forward per row
+    prefill, *suffixes = calls
+    assert prefill["cache_entry_length"] is None
+    for call in suffixes:
+        assert call["batch"] == 1
+        assert call["logits_to_keep"] == 1
+        # Deep-copied branches are already independent batch-1 rows.
+        assert call["reorders"] == 0
+        # Each branch starts at prefix length: deep-copied, not shared state.
+        assert call["cache_entry_length"] == 6
+    for result in results:
+        assert sum(result["probabilities"]) == pytest.approx(1.0)

BEL-ESPRIT-D-ACCORD-TRUST-HOLDINGS/HILBERT-PRUNED-8B

tests/test_devices.py
@@ -1,0 +1,106 @@
+from types import SimpleNamespace
+from unittest.mock import Mock
+import sys
+
+import pytest
+
+from semif_phase1.core import load_causal_model, resolve_device, synchronize
+
+
+@pytest.fixture
+def torch_stub(monkeypatch):
+    torch = SimpleNamespace(
+        cuda=SimpleNamespace(is_available=Mock(return_value=False), device_count=Mock(return_value=0),
+                             synchronize=Mock()),
+        backends=SimpleNamespace(mps=SimpleNamespace(is_available=Mock(return_value=True))),
+        mps=SimpleNamespace(synchronize=Mock()),
+        device=lambda name: SimpleNamespace(type=name.split(":")[0], name=name),
+    )
+    monkeypatch.setitem(sys.modules, "torch", torch)
+    return torch
+
+
+def test_auto_uses_mps_without_cuda(torch_stub):
+    assert resolve_device().name == "mps"
+
+
+def test_auto_preserves_single_cuda_requirement(torch_stub):
+    torch_stub.cuda.is_available.return_value = True
+    torch_stub.cuda.device_count.return_value = 1
+    assert resolve_device().name == "cuda:0"
+    torch_stub.cuda.device_count.return_value = 2
+    with pytest.raises(ValueError, match="exactly one CUDA"):
+        resolve_device()
+
+
+def test_explicit_unavailable_devices_fail(torch_stub):
+    with pytest.raises(ValueError, match="exactly one CUDA"):
+        resolve_device("cuda")
+    torch_stub.backends.mps.is_available.return_value = False
+    with pytest.raises(ValueError, match="MPS is unavailable"):
+        resolve_device("mps")
+    with pytest.raises(ValueError, match="Device must"):
+        resolve_device("cpu")
+
+
+@pytest.mark.parametrize("backend", ["cuda", "mps", "cpu"])
+def test_synchronization_dispatch(torch_stub, backend):
+    device = SimpleNamespace(type=backend)
+    synchronize(device)
+    if backend == "cuda":
+        torch_stub.cuda.synchronize.assert_called_once_with(device)
+    else:
+        torch_stub.cuda.synchronize.assert_not_called()
+    if backend == "mps":
+        torch_stub.mps.synchronize.assert_called_once_with()
+    else:
+        torch_stub.mps.synchronize.assert_not_called()
+
+
+def test_loader_passes_device_dtype_and_revision(torch_stub, monkeypatch):
+    model = Mock()
+    factory = SimpleNamespace(from_pretrained=Mock(return_value=(model, {})))
+    config = SimpleNamespace(model_type="other")
+    transformers = SimpleNamespace(
+        AutoConfig=SimpleNamespace(from_pretrained=Mock(return_value=config)),
+        AutoTokenizer=SimpleNamespace(from_pretrained=Mock(return_value=object())),
+        AutoModelForCausalLM=factory, __version__="test",
+    )
+    monkeypatch.setitem(sys.modules, "transformers", transformers)
+    torch_stub.device = lambda name: name
+    torch_stub.float16 = "float16"
+    torch_stub.__version__ = "test"
+    revision = "a" * 40
+    _, _, metadata = load_causal_model("test/model", revision, "mps", "float16")
+    kwargs = factory.from_pretrained.call_args.kwargs
+    assert kwargs["device_map"] == {"": "mps"}
+    assert kwargs["dtype"] == "float16"
+    assert kwargs["revision"] == revision
+    assert kwargs["trust_remote_code"] is False
+    assert metadata["device"] == "mps"
+    assert metadata["dtype"] == "float16"
+    model.eval.assert_called_once()
+
+
+def test_direct_scorer_synchronizes_before_and_after_forward(monkeypatch):
+    import torch
+    from semif_phase1 import direct
+
+    events = []
+
+    class Model:
+        def parameters(self):
+            yield torch.zeros(1)
+
+        def forward(self, input_ids, attention_mask, use_cache, return_dict, logits_to_keep):
+            events.append("forward")
+            return SimpleNamespace(logits=torch.tensor([[[1.0, 2.0]]]))
+
+        __call__ = forward
+
+    monkeypatch.setattr(direct, "encode_prompt", lambda *args: ([1], [0, 1], "hash"))
+    monkeypatch.setattr(direct, "synchronize", lambda device: events.append("sync"))
+    result = direct.score(Model(), None, {"id": "x", "options": [{"id": "a"}, {"id": "b"}]}, {})
+    assert events == ["sync", "forward", "sync"]
+    assert sum(result["probabilities"]) == pytest.approx(1.0)
+    assert result["option_logits"] == [1.0, 2.0]

tests/test_shared.py
@@ -1,6 +1,10 @@
+from types import SimpleNamespace
+
 import pytest
 
-from semif_phase1.shared import _suffix_layout
+torch = pytest.importorskip("torch")
+
+from semif_phase1.shared import _suffix_layout, score_shared  # noqa: E402
 
 
 def test_suffix_padding_follows_real_tokens():
@@ -14,3 +18,104 @@ def test_suffix_padding_follows_real_tokens():
 def test_empty_suffix_is_rejected():
     with pytest.raises(ValueError):
         _suffix_layout([[1], []], 7, 0)
+
+
+class FakeCache:
+    """Native-cache stand-in: mutable length, records reorder calls."""
+
+    def __init__(self, length):
+        self.length = length
+        self.orders = []
+
+    def get_seq_length(self):
+        return self.length
+
+    def reorder_cache(self, order):
+        self.orders.append(order.tolist() if hasattr(order, "tolist") else list(order))
+
+    def __deepcopy__(self, memo):
+        clone = FakeCache(self.length)
+        memo[id(self)] = clone
+        return clone
+
+
+class FakeModel:
+    """Forward with a real signature so selective-logits detection succeeds."""
+
+    def __init__(self, calls, device):
+        self.calls = calls
+        self._device = device
+
+    def parameters(self):
+        yield SimpleNamespace(device=self._device)
+
+    def eval(self):
+        return self
+
+    def forward(self, input_ids=None, attention_mask=None, use_cache=False,
+                return_dict=True, logits_to_keep=1, past_key_values=None):
+        width = int(input_ids.shape[1])
+        entry_length = None if past_key_values is None else past_key_values.length
+        self.calls.append({
+            "batch": int(input_ids.shape[0]),
+            "width": width,
+            "logits_to_keep": logits_to_keep,
+            "cache_entry_length": entry_length,
+            "reorders": None if past_key_values is None else len(past_key_values.orders),
+        })
+        if past_key_values is None:
+            past_key_values = FakeCache(width)
+        else:
+            past_key_values.length += width
+        logits = torch.arange(width + 4, dtype=torch.float32).unsqueeze(0).unsqueeze(0)
+        return SimpleNamespace(logits=logits, past_key_values=past_key_values)
+
+    __call__ = forward
+
+
+def test_score_shared_looped_path_forwards_one_row_per_call(monkeypatch):
+    if not torch.backends.mps.is_available():
+        pytest.skip("MPS unavailable")
+    rows_data = [
+        {"id": "a", "state": "s", "question": "q1", "options": [
+            {"id": "x", "description": "X"}, {"id": "y", "description": "Y"}]},
+        {"id": "b", "state": "s", "question": "q2", "options": [
+            {"id": "x", "description": "X"}, {"id": "y", "description": "Y"}]},
+    ]
+
+    class FakeTok:
+        pad_token_id = 0
+        eos_token_id = 1
+
+    calls = []
+
+    def fake_encode_prompt(tokenizer, row, max_tokens):
+        return ([1] * 8, [2, 3], f"hash-{row['id']}")
+
+    def fake_state_prefix(tokenizer, state):
+        return [1] * 6
+
+    import semif_phase1.shared as shared_module
+    monkeypatch.setattr(shared_module, "encode_prompt", fake_encode_prompt)
+    monkeypatch.setattr(shared_module, "_state_prefix", fake_state_prefix)
+    results, timing = score_shared(FakeModel(calls, torch.device("mps")), FakeTok(), rows_data, {}, 4096)
+
+    assert [r["id"] for r in results] == ["a", "b"]
+    assert timing["batch_size"] == 2
+    assert timing["suffix_forward_seconds"] > 0
+    assert timing["replicate_seconds"] > 0
+    assert all(r["model"]["serving_config"] == "native-state-prefix-looped-v1" for r in results)
+    # Looped suffixes are unpadded: padded count equals true suffix tokens.
+    assert timing["padded_suffix_tokens"] == timing["true_suffix_tokens"] == 4
+    assert len(calls) == 3  # one prefill + one suffix forward per row
+    prefill, *suffixes = calls
+    assert prefill["cache_entry_length"] is None
+    for call in suffixes:
+        assert call["batch"] == 1
+        assert call["logits_to_keep"] == 1
+        # Deep-copied branches are already independent batch-1 rows.
+        assert call["reorders"] == 0
+        # Each branch starts at prefix length: deep-copied, not shared state.
+        assert call["cache_entry_length"] == 6
+    for result in results:
+        assert sum(result["probabilities"]) == pytest.approx(1.0)